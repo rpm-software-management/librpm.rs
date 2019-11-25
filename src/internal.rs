506ff48f3e0293a1762fbdbc179bfa74b3e99b85
Michael Pankov <work@michaelpankov.com>
2019-11-25 12:35:38 +0300

    Remove global_state module

--- src/internal.rs
+++ src/internal.rs
@@ -2,11 +2,8 @@
 //!
 //! We hide the guts of how we interact with librpm until we're sure it's safe to expose
 
-pub(crate) mod global_state;
 pub(crate) mod header;
 pub(crate) mod iterator;
 pub(crate) mod tag;
 pub(crate) mod td;
 pub(crate) mod ts;
-
-pub(crate) use self::global_state::GlobalState;
--- src/internal/ts.rs
+++ src/internal/ts.rs
@@ -1,6 +1,5 @@
 //! Transaction sets: librpm's transaction API
 
-use super::GlobalState;
 use librpm_sys;
 use std::sync::atomic::AtomicPtr;
 use std::sync::MutexGuard;
@@ -36,33 +35,5 @@ impl Drop for TransactionSet {
 impl TransactionSet {
     pub(crate) fn as_mut_ptr(&mut self) -> &mut *mut librpm_sys::rpmts_s {
         self.0.get_mut()
-    }
-}
-
-/// Crate-public wrapper for acquiring and releasing the global transaction set
-/// which also cleans it prior to unlocking it.
-pub(crate) struct GlobalTS(MutexGuard<'static, GlobalState>);
-
-impl GlobalTS {
-    /// Acquire the global state mutex, giving the current thread exclusive
-    /// access to the global transaction set.
-    pub fn create() -> Self {
-        GlobalTS(GlobalState::lock())
-    }
-
-    /// Obtain the internal pointer to the transaction set
-    pub(crate) fn as_mut_ptr(&mut self) -> *mut librpm_sys::rpmts_s {
-        // Since we're guaranteed to be holding the GlobalState mutex here,
-        // we're free to deref the pointer.
-        *self.0.ts.as_mut_ptr()
-    }
-}
-
-/// Tidy up the shared global transaction set between uses
-impl Drop for GlobalTS {
-    fn drop(&mut self) {
-        unsafe {
-            librpm_sys::rpmtsClean(self.as_mut_ptr());
-        }
     }
 }
